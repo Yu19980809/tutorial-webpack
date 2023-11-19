bc011c83f5a51cb520af4dd49e7b340350de1425
Yu19980809 <1572250276@qq.com>
2023-11-19 16:20:59 +0800

    output management

--- src/index.js
+++ src/index.js
@@ -1,33 +1,16 @@
 import _ from 'lodash'
-import './style.css'
-import Icon from './webpack.jpeg'
-// import xml from './data.xml'
-// import csv from './data.csv'
-// import toml from './data.toml'
-// import yaml from './data.yaml'
-// import json from './data.json5'
-
-// console.log(toml.title)
-// console.log(toml.owner.name)
-
-// console.log(yaml.title)
-// console.log(yaml.owner.name)
-
-// console.log(json.title)
-// console.log(json.owner.name)
+import printMe from './print'
 
 function component() {
   const element = document.createElement('div')
+  const btn = document.createElement('button')
 
   element.innerHTML = _.join(['Hello', 'webpack'], '')
-  element.classList.add('hello')
 
-  const myIcon = new Image()
-  myIcon.src = Icon
-  element.appendChild(myIcon)
+  btn.innerHTML = 'Click me and check the console!'
+  btn.onclick = printMe
 
-  // console.log('XML', xml)
-  // console.log('CSV', csv)
+  element.appendChild(btn)
 
   return element
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,19 @@
 const path = require('path')
-const toml = require('toml')
-const yaml = require('yamljs')
-const json5 = require('json5')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
   },
-  module: {
-    rules: [
-      // css
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      // image
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      // font
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      // csv
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      // xml
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-      // json parser
-      {
-        test: /\.toml$/i,
-        type: 'json',
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/i,
-        type: 'json',
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        test: /\.json5$/i,
-        type: 'json',
-        parser: {
-          parse: json5.parse,
-        },
-      },
-    ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 }
