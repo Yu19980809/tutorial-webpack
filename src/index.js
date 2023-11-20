df203b597963622925e27d4ed942c565c1db0130
Yu19980809 <1572250276@qq.com>
2023-11-20 15:43:02 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import { cube } from './math.js'
-
 function component() {
-  const element = document.createElement('pre')
+  const element = document.createElement('div')
 
-  element.innerHTML = [
-    'Hello webpack!',
-    '5 cubed is equal to ' + cube(5)
-  ].join('\n\n')
+  element.innerHTML = join(['Hello', 'webpack'], ' ')
+
+  this.alert('Hmm, this probably isn\'t a gret idea...')
 
   return element
 }
 
 document.body.appendChild(component())
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(json => {
+    console.log('We retrieved some data!', json)
+  })
+  .catch(error => {
+    console.error('Something went wrong', error)
+  })
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,33 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    polyfills: './src/polyfill.js',
+    index: './src/index.js',
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
-  optimization: {
-    usedExports: true
+  module: {
+    rules: [
+      // 将 this 的指向修改为 window 对象
+      {
+        test: require.resolve('./src/index.js'),
+        use: 'imports-loader?wrapper=window',
+      },
+      // 全局导出，之后就可以使用 const { file } = require('./globl.js')
+      {
+        test: require.resolve('./src/global.js'),
+        use: 'exports-loader?type=commonjs&exports=file,multiple|helpers.parse|parse'
+      },
+    ],
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      // _: 'lodash',
+      join: ['lodash', 'join'],
+    }),
+  ],
 }
