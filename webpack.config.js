c0c35ffb218f2083a5a66bdc351e3b3225936254
Yu19980809 <1572250276@qq.com>
2023-11-20 16:00:17 +0800

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,22 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  entry: {
-    polyfills: './src/polyfill.js',
-    index: './src/index.js',
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
   module: {
     rules: [
-      // 将 this 的指向修改为 window 对象
       {
-        test: require.resolve('./src/index.js'),
-        use: 'imports-loader?wrapper=window',
-      },
-      // 全局导出，之后就可以使用 const { file } = require('./globl.js')
-      {
-        test: require.resolve('./src/global.js'),
-        use: 'exports-loader?type=commonjs&exports=file,multiple|helpers.parse|parse'
+        test: /\.(tsx|ts)$/i,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
     ],
   },
-  plugins: [
-    new webpack.ProvidePlugin({
-      // _: 'lodash',
-      join: ['lodash', 'join'],
-    }),
-  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 }
