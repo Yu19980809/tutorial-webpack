31ecdc867e10a67add3c7fecbedc254793e6e481
Yu19980809 <1572250276@qq.com>
2023-11-20 13:32:28 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-async function getComponent() {
-  const element = document.createElement('div')
-  const { default: _ } = await import('lodash')
+import _ from 'lodash'
+// import Print from './print'
 
-  element.innerHTML = _.join(['Hello', 'webpack'], '')
+function component() {
+  const element = document.createElement('div')
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  // element.onclick = Print.bind(null, 'Hello webpack!')
 
   return element
 }
 
-getComponent().then((component) => {
-  document.body.appendChild(component)
-})
+document.body.appendChild(component())
